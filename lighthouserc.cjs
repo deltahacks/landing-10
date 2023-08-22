b74d64a77d7d49903db8b67264aa0c6d7c93479f
Jason Huang <jasonhuang20035@gmail.com>
2023-08-22 00:19:07 -0400

    ci: wtf lighthouse why do you recommend everything be a pwa

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -13,7 +13,7 @@ module.exports = {
       token: "4c8a9fe6-33a7-46b1-8e2f-3bff0f514351",
     },
     assert: {
-      preset: "lighthouse:recommended",
+      preset: "lighthouse:no-pwa",
     },
   },
 };
