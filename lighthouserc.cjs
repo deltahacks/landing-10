8236552620675c3f1c385b7ab4926603256eb343
Jason Huang <jasonhuang20035@gmail.com>
2023-08-21 23:11:19 -0400

    ci: i am a dumbass

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   ci: {
     upload: {
-      target: "temporary-public-storage",
-    },
-    assert: {
       target: "lhci",
       serverBaseUrl: "https://deltahacks-lhci.up.railway.app",
       token: "7a547b5b-4683-42d0-9877-a3a8d5c52c04",
+    },
+    assert: {
+      preset: "lighthouse:recommended",
     },
   },
 };
