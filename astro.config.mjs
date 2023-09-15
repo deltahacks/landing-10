0f2119dffef14e07b73dabd5f196eaf92ceff41b
Jason Huang <jasonhuang20035@gmail.com>
2023-09-15 18:13:57 +0000

    remove astro-critters because v3 inlines css by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import critters from "astro-critters";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), critters()],
+  integrations: [tailwind()],
 });
