cedd21cb3ea0ad4e97d4e0a1c263536d1e3065b9
Sarah Simionescu <97263859+sarahsimionescu@users.noreply.github.com>
2023-09-27 19:11:33 -0400

    rmvd placeholder, added background cards, etc.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,17 @@ module.exports = {
         "dh-blue": "#2D9AEA",
       },
     },
+    textFillColor: (theme) => theme("borderColor"),
+    textStrokeColor: (theme) => theme("borderColor"),
+    textStrokeWidth: (theme) => theme("borderWidth"),
+    paintOrder: {
+      fsm: { paintOrder: "fill stroke markers" },
+      fms: { paintOrder: "fill markers stroke" },
+      sfm: { paintOrder: "stroke fill markers" },
+      smf: { paintOrder: "stroke markers fill" },
+      mfs: { paintOrder: "markers fill stroke" },
+      msf: { paintOrder: "markers stroke fill" },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-text-fill-stroke")],
 };
