0375df080ea75b1de7c2675f3ab71a74793e267f
Jason Huang <jasonhuang20035@gmail.com>
2023-09-27 16:19:27 +0000

    add base global theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dh-red": "#E72250",
+        "dh-yellow": "#F8D13D",
+        "dh-blue": "#2D9AEA",
+      },
+    },
   },
   plugins: [],
 };
