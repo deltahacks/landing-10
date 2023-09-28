f1441fd132b9345bf15fab8cce57c84fdb570650
Jason Huang <jasonhuang20035@gmail.com>
2023-09-28 14:54:28 -0400

    restore inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         "dh-red": "#E72250",
