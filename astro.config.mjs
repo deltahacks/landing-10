37e77baa9a508d9349c5294b4034c989ee97ea59
Jason Huang <jasonhuang20035@gmail.com>
2023-10-03 09:48:21 -0400

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import critters from "astro-critters";
-
 import partytown from "@astrojs/partytown";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    critters(),
-  ],
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), react(), critters()]
 });
