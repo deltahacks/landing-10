cb174bf3c72dfb46159b910971050c7c7009a238
Sarah Simionescu <97263859+sarahsimionescu@users.noreply.github.com>
2023-10-03 23:51:59 -0400

    Dynamic quesion list

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,19 +1,19 @@
 import { Disclosure, Transition } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 
-export default function Question() {
+export default function Question({ question, answer }) {
   return (
     <div className="w-full px-4 pt-16">
       <Disclosure as="div" className="mt-2">
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>Do you offer technical support?</span>
+              <span>{question}</span>
               <ChevronUpIcon className="ui-open:rotate-180 ui-open:transform w-4 transition-all" />
             </Disclosure.Button>
             <Transition
               enter="transition duration-100 ease-out"
-              enterFrom="transform scale-95 opacity-0"
+              enterFrom="transform scale-95 opacity-0 "
               enterTo="transform scale-100 opacity-100"
               leave="transition duration-75 ease-out"
               leaveFrom="transform scale-100 opacity-100"
@@ -23,7 +23,7 @@ export default function Question() {
                 static
                 className="px-4 pb-2 pt-4 text-sm text-gray-500"
               >
-                No.
+                {answer}
               </Disclosure.Panel>
             </Transition>
           </>
