94da959c5fbd0afe1c0aba6630df6ec04d947ebf
Selina Kim <selinakim343@outlook.com>
2023-10-18 21:40:26 -0400

    Improve Community & Countdown, Change font in WhoAreWe & Footer (#16)
    * fix: make coins better in mobile
    
    * fix: adjust coins z-index for navbar
    
    * fix: Community header behaviour in mobile
    
    * fix: fill in github & code of conduct urls
    
    * fix: change timer behaviour when time's up
    
    * fix: make links styling accurate to design
    
    * fix: change font in WhoAreWe
    
    * fix: change footer font to Inter
    
    * fix: remove Discord + comment Travel Guidelines
    
    * fix: change Apps open to TBA

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import dayjs, { Dayjs } from "dayjs";
+import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import React, { useMemo, useState } from "react";
 
@@ -12,16 +12,36 @@ export const Counter: React.FC<CounterProps> = ({ endTime }) => {
   const [time, setTime] = useState<number>(0);
 
   const getTimestamp = (t: number) => {
-    const twoDP = (n: number) => (n > 9 ? n : "0" + n);
+    if (t <= 0) {
+      return (
+        <div className="text-center font-bold md:col-span-2 lg:text-left">
+          <h1 className="text-2xl text-fill-transparent text-stroke-white text-stroke-2 md:whitespace-nowrap md:text-6xl">
+            <span> APPLICATIONS</span>
+            <span className="whitespace-nowrap"> OPEN</span>
+          </h1>
+          <p className="text-5xl md:whitespace-nowrap md:text-7xl">RIGHT NOW</p>
+        </div>
+      );
+    } else {
+      const twoDP = (n: number) => (n > 9 ? n : "0" + n);
 
-    var duration = dayjs.duration(t * 1000, "milliseconds");
+      var duration = dayjs.duration(t * 1000, "milliseconds");
 
-    duration = dayjs.duration(duration.asMilliseconds(), "milliseconds");
-    let timestamp = `${twoDP(duration.days() * 24 + duration.hours())}:${twoDP(
-      duration.minutes(),
-    )}:${twoDP(duration.seconds())}`;
+      duration = dayjs.duration(duration.asMilliseconds(), "milliseconds");
+      let timestamp = `${twoDP(
+        duration.days() * 24 + duration.hours(),
+      )}:${twoDP(duration.minutes())}:${twoDP(duration.seconds())}`;
 
-    return timestamp;
+      return (
+        <div className="text-center font-bold md:col-span-2 lg:text-left">
+          <h1 className="text-2xl text-fill-transparent text-stroke-white text-stroke-2 md:whitespace-nowrap md:text-6xl">
+            <span> APPLICATIONS</span>
+            <span className="whitespace-nowrap"> OPEN IN</span>
+          </h1>
+          <p className="text-7xl md:text-9xl">{timestamp}</p>
+        </div>
+      );
+    }
   };
 
   useMemo(() => {
@@ -37,5 +57,5 @@ export const Counter: React.FC<CounterProps> = ({ endTime }) => {
     return () => clearInterval(intervalId);
   }, [endTime]);
 
-  return <p>{getTimestamp(time)}</p>;
+  return getTimestamp(time);
 };
