9162e5b3eb8e21f260266146d8fc410754b9ea6c
Jason Huang <jasonhuang20035@gmail.com>
2023-10-25 20:38:54 -0400

    Revert "defer carousel load until visible"
    This reverts commit 1747fe8c868c52641307c1ee90f102a1574ce86a.

--- src/components/react/Carousel.tsx
+++ src/components/react/Carousel.tsx
@@ -3,9 +3,6 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery, { type ReactImageGalleryItem } from "react-image-gallery";
 
 export const Carousel = (props: { images: ReactImageGalleryItem[] }) => {
-  if (typeof window === "undefined") {
-    return null;
-  }
   return (
     <ImageGallery
       items={props.images}
